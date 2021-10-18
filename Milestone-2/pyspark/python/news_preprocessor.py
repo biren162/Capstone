deb6b8a4853279fabfc7c9e0f2863c78b6b66af7
Vaishali Khurana <vaishali.khurana27@gmail.com>
2021-10-18 18:09:01 +0530

    Updated data preprocessor

--- Milestone-2/pyspark/python/news_preprocessor.py
+++ Milestone-2/pyspark/python/news_preprocessor.py
@@ -2,6 +2,12 @@
 import sys
 from pyspark.sql import SQLContext, SparkSession
 from pyspark import SparkContext, SparkConf
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF, StringIndexer
+from pyspark.ml import Pipeline
+import numpy
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 sparkConf = SparkConf().setMaster("local").setAppName("MongoSparkConnectorTour").set("spark.app.id", "MongoSparkConnectorTour")
 
@@ -11,55 +17,43 @@ sqlContext = SQLContext(sc)
 
 # create and load dataframe from MongoDB URI
 df = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource")\
-                    .option("spark.mongodb.input.uri", "mongodb://admin:password@mongodb:27017/news.news_collection?authSource=admin")\
+        .option("spark.mongodb.input.uri", "mongodb://admin:password@mongodb:27017/news.news_collection?authSource=admin")\
                     .load()
 
 # print data frame schema
 df.printSchema()
+
 # print first dataframe row
-df.first()
+# df.first()
 
 # convert dataframe to rdd 
-rdd = df.rdd 
-rdd.first()
+# rdd = df.rdd 
+# rdd.first()
 
-# Read through using aggregation pipeline
-# pipeline = [{'$sort': {'timestamp': 1}},
-#             {'$group':{'_id':{'myid':'$myid'}, 'record':{'$first':'$$ROOT'}}}, 
-#             {'$project':{'_id':0, 'doc':'$record.doc', 'timestamp':'$record.timestamp', 'myid':'$record.myid'}}
-#             ]
-# df_pipeline = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource")\
-#                              .option("spark.mongodb.input.uri", "mongodb://mongodb:27017/spark.times")\
-#                              .option("pipeline", pipeline).load()
-# df_pipeline.first()
+print('cleaning start')
 
-# # Filter by Integer and by String
-# df.filter(df["myid"] < 2).show()
-# df.filter(df["doc"] == "V ").show()
+print(df.columns)
 
-# # DataFrames SQL example 
-# df.registerTempTable("temporary")
-# sqlResult = sqlContext.sql("SELECT myid, doc, timestamp FROM temporary WHERE myid > 6 AND doc='V '")
-# sqlResult.show()
+stopwords = stopwords.words('english')
+tokenizer = Tokenizer(inputCol="summary", outputCol="words")
+stopwordsRemover = StopWordsRemover(inputCol="words", outputCol="filtered_data").setStopWords(stopwords)
+hashingTF = HashingTF(inputCol="filtered_data", outputCol="tf", numFeatures=10000)
+idf = IDF(inputCol="tf", outputCol="idf", minDocFreq=5)
+labelAnnotator = StringIndexer(inputCol = "category", outputCol = "label")
+preprocessorPipeline = Pipeline(stages=[tokenizer, stopwordsRemover, hashingTF, idf, labelAnnotator])
+preprocessorPipelineFit = preprocessorPipeline.fit(df)
 
-# # Save out the filtered DataFrame result
-# sqlResult.write.format("com.mongodb.spark.sql.DefaultSource")\
-#         .option("spark.mongodb.output.uri", "mongodb://mongodb:27017/spark.output")\
-#         .mode("overwrite")\
-#         .save()
+#preprocessorPipelineFit.save('preprocessor')
+#preprocessor = Pipeline.load("preprocessor")
 
-# # Read it back in to confirm 
-# df = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource")\
-#                     .option("spark.mongodb.input.uri", "mongodb://mongodb:27017/spark.output")\
-#                     .load()
-# df.show()
+cleaned_df = preprocessorPipelineFit.transform(df)
+#cleaned_df = preprocessor.transform(df)
 
-# # Read a csv file and store into MongoDB 
-# spark = SparkSession.builder.appName("MongoSparkConnectorTour").getOrCreate()
-# dataframe = spark.read.csv("./population_by_country_1980_2010.csv", header=True, mode="DROPMALFORMED")
-# dataframe.write.format("com.mongodb.spark.sql.DefaultSource")\
-#                .option("spark.mongodb.output.uri", "mongodb://mongodb:27017/spark.population")\
-#                .save()
+print('cleaning done')
+print(cleaned_df.columns)
+print(cleaned_df.show(5))
+
+
 
 print("Done")
 sys.exit(0)
