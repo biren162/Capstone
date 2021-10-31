05ab4ee5dfcd71e7ed5d5b3949e312bd6e7c8e6e
Vaishali Khurana <vaishali.khurana27@gmail.com>
2021-10-31 20:32:05 +0530

    Load model during application startup

--- NewsClassifier/Prediction/ml_utils.py
+++ NewsClassifier/Prediction/ml_utils.py
@@ -4,26 +4,32 @@ import torch
 from tensorflow.keras.models import load_model
 import bert
 
+
 def init_app():
     print('Initializing the things...')
+    global saved_tokenizer
+    saved_tokenizer = torch.load('models/tokenizer')
+    global saved_model
+    saved_model = load_trained_model()
 
 
 def load_trained_model():
-    return load_model("models/bert_model.h5",custom_objects={"BertModelLayer": bert.BertModelLayer})
+    return load_model("models/bert_model.h5", custom_objects={"BertModelLayer": bert.BertModelLayer})
 
 # function to predict the class
+
 
 def predict(text):
     max_seq_len = 512
     classes = ['Science/Technology', 'Sports', 'Politics', 'Business']
-    saved_tokenizer = torch.load('models/tokenizer')
-    saved_model = load_trained_model()
     sentences = [text]
     pred_tokens = map(saved_tokenizer.tokenize, sentences)
     pred_tokens = map(lambda tok: ["[CLS]"] + tok + ["[SEP]"], pred_tokens)
-    pred_token_ids = list(map(saved_tokenizer.convert_tokens_to_ids, pred_tokens))
+    pred_token_ids = list(
+        map(saved_tokenizer.convert_tokens_to_ids, pred_tokens))
 
-    pred_token_ids = map(lambda tids: tids + [0] * (max_seq_len - len(tids)), pred_token_ids)
+    pred_token_ids = map(lambda tids: tids +
+                         [0] * (max_seq_len - len(tids)), pred_token_ids)
     pred_token_ids = np.array(list(pred_token_ids))
 
     predictions = saved_model.predict(pred_token_ids).argmax(axis=-1)
